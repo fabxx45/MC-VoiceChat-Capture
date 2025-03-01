a7f9b990c46bb97dab46c4a94a790051ce5d7da8
fabxx45 <fabian.hammer2709@gmail.com>
2025-03-01 12:46:57 +0100

    some changes

--- VoiceChatListenerPlugin.java
+++ VoiceChatListenerPlugin.java
@@ -13,6 +13,11 @@ import java.util.function.Consumer;
 public class VoiceChatListenerPlugin implements VoicechatPlugin {
 
     @Override
+    public String getPluginId() {
+        return "voice_chat_listener_plugin";
+    }
+
+    @Override
     public void registerEvents(EventRegistration registration) {
         registration.registerEvent(VoicechatServerStartedEvent.class, this::onVoicechatServerStarted);
     }
@@ -23,17 +28,17 @@ public class VoiceChatListenerPlugin implements VoicechatPlugin {
         // Example: Listen to a specific player's voice packets
         UUID playerUuid = UUID.fromString("player-uuid-here"); // Replace with the actual player UUID
 
-        PlayerAudioListener listener = api.createPlayerAudioListenerBuilder()
+        PlayerAudioListener listener = api.playerAudioListenerBuilder()
                 .setPlayer(playerUuid)
                 .setPacketListener(this::handleSoundPacket)
                 .build();
 
-        api.registerPlayerAudioListener(listener);
+        api.registerAudioListener(listener);
     }
 
     private void handleSoundPacket(SoundPacket soundPacket) {
         // Handle the incoming sound packet
-        System.out.println("Received sound packet from player: " + soundPacket.getSender().getName());
+        System.out.println("Received sound packet from player: " + soundPacket.getSender().getClass());
         // You can process the sound packet data here
     }
 }
